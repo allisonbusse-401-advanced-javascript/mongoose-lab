e908e9e8577d5de56493d0eb20268cda656efe95
allisonbusse <allisonbusse@gmail.com>
2019-09-23 16:54:12 -0700

    finished routes

--- server.js
+++ server.js
@@ -14,4 +14,40 @@ app.get('/api/mountains', (req, res, next) => {
     .catch(next);
 });
 
+app.get('/api/mountains/:id', (req, res, next) => {
+  Mountain.findById(req.params.id)
+    .then(mountain => {
+      res.json(mountain);
+    })
+    .catch(next);
+});
+
+app.post('/api/mountains', (req, res, next) => {
+  Mountain.create(req.body)
+    .then(mountain => {
+      res.json(mountain);
+    })
+    .catch(next);
+});
+
+app.put('/api/mountains/:id', (req, res, next) => {
+  Mountain.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+    .then(mountain => {
+      res.json(mountain);
+    })
+    .catch(next);
+});
+
+app.delete('/api/mountains/:id', (req, res, next) => {
+  Mountain.findByIdAndRemove(req.params.id)
+    .then(removed => {
+      res.json(removed);
+    })
+    .catch(next);
+});
+
 app.listen(3000, () => console.log('server running on 3000'));

654dfd7a902bb616818c5afa326087ec92884c36
allisonbusse <allisonbusse@gmail.com>
2019-09-25 15:27:35 -0700

    wrote everything

--- server.js
+++ server.js
@@ -1,53 +1,12 @@
 require('dotenv').config();
-require('./lib/connect')();
-const express = require('express');
-const app = express();
-const Mountain = require('./lib/models/mountains');
 
-app.use(express.json());
+require('./lib/connect')(process.env.MONGODB_URI);
 
-app.get('/api/mountains', (req, res, next) => {
-  Mountain.find()
-    .then(mountains => {
-      res.json(mountains);
-    })
-    .catch(next);
+const app = require('./lib/app');
+const { createServer } = require('http');
+const server = createServer(app);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
-
-app.get('/api/mountains/:id', (req, res, next) => {
-  Mountain.findById(req.params.id)
-    .then(mountain => {
-      res.json(mountain);
-    })
-    .catch(next);
-});
-
-app.post('/api/mountains', (req, res, next) => {
-  Mountain.create(req.body)
-    .then(mountain => {
-      res.json(mountain);
-    })
-    .catch(next);
-});
-
-app.put('/api/mountains/:id', (req, res, next) => {
-  Mountain.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  )
-    .then(mountain => {
-      res.json(mountain);
-    })
-    .catch(next);
-});
-
-app.delete('/api/mountains/:id', (req, res, next) => {
-  Mountain.findByIdAndRemove(req.params.id)
-    .then(removed => {
-      res.json(removed);
-    })
-    .catch(next);
-});
-
-app.listen(3000, () => console.log('server running on 3000'));
